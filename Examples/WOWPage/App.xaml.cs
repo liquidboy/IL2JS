76a3517aa5df20ddcea6ac6caac678cd507ee343
Jose Fajardo <fajardo_jf@hotmail.com>
2014-02-22 07:25:04 +1100

    example : WOWPage - start
    Starting work on my own example, still getting head around concepts so
    this example will evolve over time

---
+++ Examples/WOWPage/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using Microsoft.LiveLabs.JavaScript.IL2JS;
+using Microsoft.LiveLabs.JavaScript.ManagedInterop.Silverlight;
+
+namespace WOWPage
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+            this.Exit += this.Application_Exit;
+            this.UnhandledException += this.Application_UnhandledException;
+
+            InitializeComponent();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //this.RootVisual = new MainPage();
+
+            // Initialize the bridge to JavaScript and, when done, launch the application's main page.
+            SilverlightBridge.Initialize(
+                true,   // SECURITY:  true if we trust the JavaScript engine to call back into the
+                //            Silverlight runtime
+                true    // DEBUGGING: true if we wish to capture all communication between the Silverlight and
+                //            JavaScript engines in any exception.
+            );
+
+            WOWPageStart.Run();
+        }
+
+        private void Application_Exit(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            // If the app is running outside of the debugger then report the exception using
+            // the browser's exception mechanism. On IE this will display it a yellow alert 
+            // icon in the status bar and Firefox will display a script error.
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+
+                // NOTE: This will allow the application to continue running after an exception has been thrown
+                // but not handled. 
+                // For production applications this error handling should be replaced with something that will 
+                // report the error to the website and stop the application.
+                e.Handled = true;
+                Deployment.Current.Dispatcher.BeginInvoke(delegate { ReportErrorToDOM(e); });
+            }
+        }
+
+        private void ReportErrorToDOM(ApplicationUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                string errorMsg = e.ExceptionObject.Message + e.ExceptionObject.StackTrace;
+                errorMsg = errorMsg.Replace('"', '\'').Replace("\r\n", @"\n");
+
+                System.Windows.Browser.HtmlPage.Window.Eval("throw new Error(\"Unhandled Error in Silverlight Application " + errorMsg + "\");");
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Examples/WOWPage/OpenJS/BrowserUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.LiveLabs.Html;
+using Microsoft.LiveLabs.JavaScript;
+using Microsoft.LiveLabs.JavaScript.Interop;
+
+namespace WOWPage.OpenJS
+{
+    internal static class BrowserUtility
+    {
+        public static void InitBrowserUtility()
+        {
+            FireFox = false;
+            InternetExplorer = false;
+            InternetExplorer7 = false;
+            InternetExplorer8Standard = false;
+
+            string agt = Browser.Window.Navigator.UserAgent.ToLower();
+            InternetExplorer = (agt.IndexOf("msie") != -1);
+            if (InternetExplorer)
+            {
+                string stIEVer = agt.Substring(agt.IndexOf("msie ") + 5);
+                var j = stIEVer.IndexOf(';');
+                if (j > 0)
+                    stIEVer = stIEVer.Substring(0, j);
+                var v = default(double);
+                if (double.TryParse(stIEVer, out v))
+                    InternetExplorer7 = v == 7.0;
+
+                // In IE8, the version number, more specifically the number follows msie in userAgent string is not necessary 8,
+                // Thus we should really look at documentMode to see how the page is rendered. The documentMode can be 8,7,5
+                InternetExplorer8Standard = Browser.Document.DocumentMode == 8;
+            }
+
+            bool nav = ((agt.IndexOf("mozilla") != -1) && ((agt.IndexOf("spoofer") == -1) && (agt.IndexOf("compatible") == -1)));
+            FireFox = nav && (agt.IndexOf("firefox") != -1);
+        }
+
+        public static bool FireFox { get; private set; }
+        public static bool InternetExplorer { get; private set; }
+        public static bool InternetExplorer7 { get; private set; }
+        public static bool InternetExplorer8Standard { get; private set; }
+    }
+}
