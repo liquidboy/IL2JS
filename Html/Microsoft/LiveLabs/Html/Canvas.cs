fc40fab6025ecf163e418f13595be67a1d721a05
Jose Fajardo <fajardo_jf@hotmail.com>
2014-03-17 16:44:40 +1100

    DirectCanvas - renderloop and resize

--- Html/Microsoft/LiveLabs/Html/Canvas.cs
+++ Html/Microsoft/LiveLabs/Html/Canvas.cs
@@ -10,5 +10,19 @@ namespace Microsoft.LiveLabs.Html
         [Import(@"function() { return document.createElement(""Canvas""); }")]
         extern public Canvas();
 
+        [Import("function(inst) { return inst.getContext('2d'); }", PassInstanceAsArgument = true)]
+        extern public Context2D Context2D();
+
+    }
+
+
+    [Import]
+    public class Context2D
+    {
+        [Import("fillStyle")]
+        extern public string FillStyle { get; set; }
+
+        [Import("fillRect")]
+        extern public string FillRect(int x, int y, int w, int h);
     }
 }
